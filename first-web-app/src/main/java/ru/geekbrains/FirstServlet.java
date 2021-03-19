6bf43e55b57e72c72c8a49ee2a3813e8d0a7e0a5
EvgenySaenko <storn84@mail.ru>
2021-03-19 11:30:43 +0300

    added entity Product and ProductRepository added  ProductServlet implemented a product search by id at get request of the form "/first-web-app/product/2"

--- first-web-app/src/main/java/ru/geekbrains/FirstServlet.java
+++ first-web-app/src/main/java/ru/geekbrains/FirstServlet.java
@@ -6,10 +6,14 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class FirstServlet implements Servlet {
     private static final Logger logger = LoggerFactory.getLogger(FirstServlet.class);
     private ServletConfig config;
+
+    private List<String> sharedList = new CopyOnWriteArrayList<>();
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -23,11 +27,14 @@ public class FirstServlet implements Servlet {
         return this.config;
     }
 
-    @Override
+    @Override//вызывается при каждом запросе
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         logger.info("New request");
 
+        sharedList.add("asdada");
+
         res.getWriter().println("<h1>Hello from Servlet!!!</h1>");
+        res.getWriter().println("<h2>Привет!!!</h2>");
     }
 
     @Override
